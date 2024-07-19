896f1db6424365a3d464114ad83ab978ae181883
laibayounus59 <laibayounus27@gmail.com>
2024-07-19 23:20:53 +0500

    Add files via upload

--- Snake and Ladder game.py
+++ Snake and Ladder game.py
@@ -1,8 +1,7 @@
 import random
 snakes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
 ladders = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
-def print_board(p
-                layer1, player2):
+def print_board(player1, player2):
     print(f"Player 1 is at position {player1}")
     print(f"Player 2 is at position {player2}")
 def check_snakes_ladders(position):
